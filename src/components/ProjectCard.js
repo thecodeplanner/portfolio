8473f49531bfce8028e13059561a5d2db5a1319d
thecodeplanner <thecodeplanner@gmail.com>
2021-03-13 02:14:03 -0500

    added content to projects page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,10 +5,11 @@ import React, {useState} from "react"
 function ProjectCard({project}) {
 
     return (
-        <div className='ui raised segment'>
+        <div className=' projects ui raised segment' style={{backgroundColor:'#faf3e0'}} >
             <h2 className='proj-name'> {project.name} </h2>
             <p className='text-content'><a className='link' target='_blank' href={project.github}>GitHub</a> | <a className='link' target='_blank' href={project.video}>Demo</a></p>
             <p className='description'>{project.description}</p>
+
             <img className='screenshots' src={project.image[0]}></img>
             <img className='screenshots' src={project.image[1]}></img>
             <img className='screenshots' src={project.image[2]}></img>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,8 @@ function Project() {
 
     
     return (
-        <div className='projects'>
+        <div className='ui raised segment ' style={{backgroundColor:'#eabf9f'}}>
+            <p className='intro'>Below are a few of my projects I've created that share my interests. With my love for health and wellness combined with a focus on building clean and minimalistic web applications, I strive to work for a company whose mission aligns with my passions. Please feel free to view my GitHub repositories and demo videos to learn more about each app.</p>
             {project}
         </div>
     )
