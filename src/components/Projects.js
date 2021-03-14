88799f3e65d5178090438f03ff9b7050f3cedf2f
thecodeplanner <thecodeplanner@gmail.com>
2021-03-13 20:14:59 -0500

    updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,7 +15,7 @@ function Project() {
         <>
         <div className='ui raised segment ' style={{backgroundColor:'#eabf9f'}}>
             <div className='intro'>
-                <h1 className='title'>View my work!</h1>
+                <h1 className='title'>Check out my work!</h1>
                 <p className='intro' >Below are a few projects I created that show more about me and my interests. With my love for health and wellness combined with a focus on building clean and minimalistic web applications, I strive to work for a company whose mission aligns with my passions. <br></br><br></br> Please feel free to view my GitHub repositories and demo videos to learn more about each application.</p>
             </div>
             </div>
--- src/components/data.js
+++ src/components/data.js
@@ -2,21 +2,21 @@
 export const projectArray = [
     {
         name: 'Remindful',
-        description: 'Self-care journaling app made for users to keep track of their goals including their thoughts and feelings each day',
+        description: 'Self-care journaling app made for users to keep track of their goals including their thoughts and feelings each day.',
         github: 'https://github.com/thecodeplanner/remindful-frontend',
         image: ['remindful1.png', 'remindful2.png', 'remindful3.png'],
         video: 'https://www.youtube.com/watch?v=xM5kdWkimJo',
     }, 
     {
         name: 'Freshmade Market',
-        description: 'Online market where users can browse, purchase and sell homemade and organic foods from around the world',
+        description: 'Online market where users can browse, purchase and sell homemade and organic foods from around the world.',
         github: 'https://github.com/thecodeplanner/fresh-market',
         image: ['freshmade1.png', 'freshmade2.png', 'freshmade3.png'],
         video: 'https://www.youtube.com/watch?v=C3uAkHAwf94',
     }, 
     {
         name: 'HomeStory',
-        description: 'App made for users to store furniture items and curate ideas for different rooms in their home',
+        description: 'App made for users to store furniture items and curate ideas for different rooms in their home.',
         github: 'https://github.com/matthewogtong/homestory-frontend',
         image: ['HomeStory1.png', 'HomeStory2.png', 'HomeStory3.png'],
         video: 'https://www.youtube.com/watch?v=z2FRG7H13aY',
